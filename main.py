0827e3a6240959742b4144b1bb7f8222bf1f4759
Gunjan <gunjan@fusemachines.com>
2023-08-03 09:02:21 +0545

    Adding main.py to the stage with add, sub, and mul functions

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+
+if __name__ == "__main__":
+    print("Addition:", add(7,5))
+    print("Subtraction:", sub(7,5))
+    print("Multiplication:", mul(7,5))
+    
